ebe7ffa22f60a05d2aa5c46e1d85ac6bc521ae3d
ThanhTien <thanhtien2094@gmail.com>
2023-09-27 22:45:51 +0700

    update

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+  },
   envPrefix: "REACT_APP_",
    build: {
     outDir: "dist",
